b253e7e70e887dc25023fa8d6d7227485e1ebf13
Pierre David <pierreaymeric.david@gmail.com>
2020-11-16 16:43:13 +0100

    bug correction

--- app/models/coder.rb
+++ app/models/coder.rb
@@ -3,7 +3,7 @@ class Coder < ApplicationRecord
   has_many :missions
 
   validates :price_per_day, presence: true, inclusion: { in: (1..10000)}
-  validates :email, format: { with: /\A.@.\.com\z/, message: "email valide uniquement" }
+  validates :email, format: { with: /^.@.\.com\z$/, message: "email valide uniquement" }
   validates :phone_number, format: { with: /^(\+33\s[1-9]{8})|(0[1-9]\s{8})$/, message: "uniquement + ou des nombres" }
   validates :description,  length: { minimum: 100 }
 end
