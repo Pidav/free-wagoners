c74311991a001655390e3b52244a8edb11e6e2c9
Pierre David <pierreaymeric.david@gmail.com>
2020-11-17 14:46:49 +0100

    title and description

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -2,6 +2,7 @@ class Mission < ApplicationRecord
   belongs_to :user
   belongs_to :coder
 
+  validates :title, presence: true
   # validates :start_date, presence: true
   # validates :end_date, presence: true, date: { after_or_equal_to: :start_date }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_151928) do
+ActiveRecord::Schema.define(version: 2020_11_17_132530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2020_11_16_151928) do
     t.boolean "validated_mission"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.text "description"
     t.index ["coder_id"], name: "index_missions_on_coder_id"
     t.index ["user_id"], name: "index_missions_on_user_id"
   end
