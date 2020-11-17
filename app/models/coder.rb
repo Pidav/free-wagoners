785bc78514f2b536279238a3f51b57ed301f996c
Pierre David <pierreaymeric.david@gmail.com>
2020-11-17 11:05:54 +0100

    cleaning seeds

--- app/models/coder.rb
+++ app/models/coder.rb
@@ -4,8 +4,8 @@ class Coder < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :price_per_day, presence: true, inclusion: { in: (1..10_000) }
-  # validates :email, format: { with: /\A.@.\.com\z/, message: "email valide uniquement" }
+  validates :email, format: { with: /\A([^@]+)@([^@]+)\.(\w+)\z/, message: "email valide uniquement" }
   validates :phone_number, presence: true
-  # validates :phone_number, format: { with: /\A(\+33\s[1-9]{9})|(0[1-9]\s{10})\z/, message: "uniquement + ou des nombres" }
+  validates :phone_number, format: { with: /\A(0|\+33[\s-]?)[1-9][\s-]?(\d{2}[\s-]?){4}\z/, message: "uniquement + ou des nombres" }
   validates :description,  length: { minimum: 100 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,56 +8,61 @@
 require 'faker'
 require 'date'
 
-5.times do
-  puts "creating user"
+def user_creation
   new_user = User.new(
-    email: Faker::Internet.unique.free_email,
-    password: "12345678",
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    company_name: Faker::Company.name,
-    description: Faker::Lorem.sentence(word_count: 15),
-    phone_number: "+33 #{Faker::PhoneNumber.subscriber_number(length: 9)}"
-    )
-  new_user.save
-  puts new_user
+  email: Faker::Internet.unique.free_email,
+  password: "12345678",
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  company_name: Faker::Company.name,
+  description: Faker::Lorem.sentence(word_count: 15 + rand() * 20),
+  phone_number: "+33 #{[1,6,7].sample}#{Faker::PhoneNumber.subscriber_number(length: 8)}"
+  )
+  if !new_user.save
+    puts "erreur user"
+    p new_user
+  end
+  return new_user
+end
+
+def coder_creation(user)
   new_coder = Coder.new(
     name: Faker::Name.unique.name,
-    price_per_day: (rand()*10).round * 100,
+    price_per_day: ((rand() * 10).round + 1)  * 100,
     email: Faker::Internet.unique.free_email,
-    description: Faker::Lorem.sentence(word_count: 25),
-    phone_number: "+33 #{Faker::PhoneNumber.subscriber_number(length: 9)}",
+    description: Faker::Lorem.sentence(word_count: 25 + rand() * 20 ),
+    phone_number: "+33 #{[1,6,7].sample}#{Faker::PhoneNumber.subscriber_number(length: 8)}",
     )
-  new_coder.user = new_user
-  new_coder.save
-  puts "coder created"
-  puts new_coder
-  4.times do
+  new_coder.user = user
+  if !new_coder.save
+    puts "erreur coder"
     p new_coder
-    duration = ((rand() * 10).round * 7)
+  end
+  return new_coder
+end
+
+15.times do
+  new_user = user_creation
+  new_coder = coder_creation(new_user)
+
+  4.times do
+    duration = ((rand() * 10).round * 7) + 1
     valid = (rand() > 0.5)
+    date_start = Date.new(2020,6+(rand()*6).round,rand()*28.round+1)
+    date_end = date_start + duration
+    unless (date_start.nil? || date_end.nil?)
     new_mission = Mission.new(
-      start_date: Date.today,
-      end_date: Date.today + duration,
-      total_price: (rand()*10).round * 100,
+      start_date: date_start,
+      end_date: date_end + duration,
+      total_price: ((rand()*9).round + 1 * 100) * duration,
       validated_mission: valid
       )
     new_mission.coder = new_coder
-    puts new_mission.coder
-    new_user_for_mission = User.new(
-      email: Faker::Internet.unique.free_email,
-      password: "12345678",
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      company_name: Faker::Company.name,
-      description: Faker::Lorem.sentence(word_count: 15),
-      phone_number: "+33 #{Faker::PhoneNumber.subscriber_number(length: 9)}"
-      )
-    new_user_for_mission.save
+    new_user_for_mission = user_creation
     new_mission.user = new_user_for_mission
-    puts new_mission
-    p new_mission
+    # p new_mission
     new_mission.save
+    end
   end
 
 end
