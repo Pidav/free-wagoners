7078f881fadde86b08cdc71a6b3f973f0009f2bb
Pierre David <pierreaymeric.david@gmail.com>
2020-11-20 16:17:35 +0100

    bug fix 1

--- app/models/coder.rb
+++ app/models/coder.rb
@@ -12,7 +12,7 @@ CODE_LIST = [ "Ruby", "Python", "Javascript", "HTML", "CSS", "ReactJS", "VueJS",
   validates :email, format: { with: /\A([^@]+)@([^@]+)\.(\w+)\z/, message: "email valide uniquement" }
   validates :phone_number, presence: true
   validates :phone_number, format: { with: /\A(0|\+33[\s-]?)[1-9][\s-]?(\d{2}[\s-]?){4}\z/, message: "uniquement + ou des nombres" }
-  validates :description,  length: { minimum: 100 }
+  validates :description,  length: { minimum: 10 }
 
   include PgSearch::Model
   pg_search_scope :global_search,
